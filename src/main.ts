ef7badb2f35086ceef60f628608e99817d09718d
ykumar23 <54766690+ykumar23@users.noreply.github.com>
2025-06-27 07:37:04 +0000

    modifiied files

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,6 @@ import { bootstrapApplication } from '@angular/platform-browser';
   selector: 'app-root',
   template: `
     <h1>Hello from {{ name }}!</h1>
-    <a target="_blank" href="https://angular.dev/overview">
-      Learn more about Angular
-    </a>
   `,
 })
 export class App {
